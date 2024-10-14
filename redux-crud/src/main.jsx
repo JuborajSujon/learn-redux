84284d322e1a63f2aede9fb6de718cff0fbb8e5d
Juboraj Sujon <mdsujon72@gmail.com>
2024-10-14 17:54:38 +0600

    inital redux store and add provider with app

--- redux-crud/src/main.jsx
+++ redux-crud/src/main.jsx
@@ -1,19 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-    </div>
+    <Provider store={store}>
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </Provider>
   </StrictMode>
 );
