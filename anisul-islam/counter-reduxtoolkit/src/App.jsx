f948d6fefb077ca697aeff2f330723923c14d97a
Juboraj Sujon <mdsujon72@gmail.com>
2024-10-14 12:55:56 +0600

    understand about redux toolkit

--- anisul-islam/counter-reduxtoolkit/src/App.jsx
+++ anisul-islam/counter-reduxtoolkit/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import CounterView from "./features/counter/CounterView";
 
 function App() {
   return (
     <>
       <h1>Counter App - Redux Toolkit</h1>
+      <CounterView />
     </>
   );
 }
