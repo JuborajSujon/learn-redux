8640b7067216bebb9a256714a57b4ba553013f89
Juboraj Sujon <mdsujon72@gmail.com>
2024-10-14 18:52:00 +0600

    Added layout and component

--- redux-crud/src/main.jsx
+++ redux-crud/src/main.jsx
@@ -1,15 +1,31 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./layout/Main";
+import Home from "./pages/Home";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </div>
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
---
+++ redux-crud/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import useScrollPosition from "./../hook/useScrollPosition";
+
+const Home = () => {
+  useScrollPosition();
+  return <div>This Main Page</div>;
+};
+
+export default Home;
